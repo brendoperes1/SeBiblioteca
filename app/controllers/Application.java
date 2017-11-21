bae53d96e75e0b2260f6c1347d93c9aaf5f2c5ae
hiraaki <mauricio_ishida@hotmail.com>
2017-11-21 20:42:37 -0200

    index pegando os valores corretos

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,6 +11,7 @@ import views.html.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Application extends Controller {
 
@@ -21,7 +22,7 @@ public class Application extends Controller {
     /*0-9 v <volume>
     * digito 10 ou + é livro
     * */
-    public Result autores(){
+    public Result Autores(){
         JsonNode json = request().body().asJson();
 
         if(json == null) {
@@ -31,36 +32,113 @@ public class Application extends Controller {
             int paginas = json.findPath("npaginas").asInt();
 
             List<Livro> livros = Ebean.find(Livro.class)
+                    //.setDistinct(true)
                     .where()
                     .like("desc_area_conhecimento",genero)
                     .findList();
+
+
+            //List<Livro> autores = new ArrayList<Livro>();
             List<String> autores = new ArrayList<String>();
+            List<String> Saida=new ArrayList<String>();
             System.out.println(json.toString());
             for (Livro L: livros) {
-
                 String pg = L.pagina;
-
                 if (pg!=null) {
                    // System.out.println(pg);
                     if ((pg.charAt(pg.length() - 1) == '.') && (pg.charAt(pg.length() - 2) == 'p')) {
-                        if ((pg.length() <= 6)) {
-                            if (paginas < 200) {
+                        if ((pg.length() <= 5)) {
+                            if (paginas < 100) {
                                 autores.add(L.autor);
-                            } else if ((pg.charAt(0) == '2') || (pg.charAt(0) == '3')) {
-                                autores.add(L.autor);
-                            } else if (pg.charAt(0) == '4') {
-                                autores.add(L.autor);
+                               // autores.add(L);
                             }
-                        } else if (paginas > 999) {
+                        }else if ((pg.length()==6)) {
+                            if((paginas >= 100)&&(paginas < 300)){
+                                if((pg.charAt(0) == '1') || (pg.charAt(0) == '2'))
+                                    autores.add(L.autor);
+                                    //autores.add(L);
+
+                            }else if((paginas >= 300)&&(paginas < 400))
+                                if((pg.charAt(0) == '3') || (pg.charAt(0) == '4'))
+                                    autores.add(L.autor);
+                                    //autores.add(L);
+
+
+                        }else if((pg.charAt(0)!=' ')&&(paginas>=500)){
+                            //autores.add(L);
                             autores.add(L.autor);
                         }
+                        if((pg.charAt(0)==' ')&&(paginas<500)){
+                            if ((pg.length() <= 6)) {
+                                if (paginas < 100) {
+                                    autores.add(L.autor);
+                                    //autores.add(L);
+                                }
+                            } else if ((pg.length() == 7)) {
+                                if ((paginas >= 100) && (paginas < 300)) {
+                                    if ((pg.charAt(0) == '1') || (pg.charAt(0) == '2'))
+                                        //autores.add(L);
+                                        autores.add(L.autor);
+                                }
+                                if ((paginas >= 300) && (paginas < 400))
+                                    if ((pg.charAt(0) == '3') || (pg.charAt(0) == '4'))
+                                        //autores.add(L);
+                                        autores.add(L.autor);
 
+                            }
+                        }
                     }
+
+                    String aux=new String();
+                    for (int i=0;i<autores.size();i++){
+                        if(!aux.equals(autores.get(i))){
+                            aux=autores.get(i);
+
+                        }else if(aux.equals(autores.get(i))){
+                            autores.remove(i);
+                        }
+                        for(int j=i+1;j<autores.size();j++){
+                            if(aux.equals(autores.get(j))){
+                                autores.remove(j);
+                            }
+                        }
+                    }
+
+//                                else if(pg.charAt(0) == ' ') {
+//                                    if ((pg.charAt(1) == '1') || (pg.charAt(1) == '2') ) {
+//                                        autores.add(L);
+//                                    }
+//                        else if (pg.charAt(1) == '4') {
+//                            //autores.add(L.autor);
+//                            autores.add(L);
+//                        }else if (paginas > 999) {
+//                            //autores.add(L.autor);
+//                            autores.add(L);
+//                        }
+
+
                 }
 //                autores.add(pg);
             }
+            if(autores.size()>20){
+                Random random = new Random();
+                int nAleatorio[] = new int[20];
+
+                for(int i=0;i<20;i++){
+                    nAleatorio[i] = random.nextInt(autores.size());
+                    for(int j=i;j>=0;j--){
+                        if(nAleatorio[i]==nAleatorio[j]){
+                            nAleatorio[i] = random.nextInt(autores.size());
+                        }
+                    }
+                    Saida.add(autores.get(nAleatorio[i]));
+                    System.out.println(Saida.get(i));
+                    System.out.println(nAleatorio[i]);
+                }
+            }
+            return ok(Json.toJson(Saida));
         }
-        return ok(Json.toJson(autores()));
+
 
     }
 }
--- public/javascripts/LiteraturaEstrangeira.js
+++ public/javascripts/LiteraturaEstrangeira.js
@@ -1,21 +1,18 @@
-function SelecionaAutores() {
-    console.log("foi")
-    var listaDeAutores = [];
-    var cont = 0;
-    //console.log(autor.size)
-
-    for(var i = 0; i < 9; i++){
-        console.log(autor[i])
-        if(autor[i] != null) {
-            listaDeAutores[cont] = autor[i];
-            cont++;
-        }
-    }
-    for(var i=0;i<9;i++ ){
-        console.log(listaDeAutores[i]);
-    }
-
-}
-function PesquisaAutor() {
-    
-}
+// function SelecionaAutores() {
+//     console.log("foi")
+//     var listaDeAutores = [];
+//     var cont = 0;
+//     //console.log(autor.size)
+//
+//     for(var i = 0; i < 9; i++){
+//         console.log(autor[i])
+//         if(autor[i] != null) {
+//             listaDeAutores[cont] = autor[i];
+//             cont++;
+//         }
+//     }
+//     for(var i=0;i<9;i++ ){
+//         console.log(listaDeAutores[i]);
+//     }
+//
+// }
